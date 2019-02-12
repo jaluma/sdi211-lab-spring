84843fd679ef7f052cfba21eb657a94979eb834e
Javi Martinez <javiermartinezalvarez98@gmail.com>
2019-02-12 17:50:22 +0100

    SDI-SpringBoot- 5 - Autenticación y control de acceso
    Signed-off-by: Javi Martinez <javiermartinezalvarez98@gmail.com>

--- Notaneitor/src/main/java/com/uniovi/services/InsertSampleDataService.java
+++ Notaneitor/src/main/java/com/uniovi/services/InsertSampleDataService.java
@@ -3,12 +3,13 @@ package com.uniovi.services;
 import com.uniovi.entities.Mark;
 import com.uniovi.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.HashSet;
 import java.util.Set;
 
-//@Service
+@Service
 public class InsertSampleDataService {
 	@Autowired
 	private UsersService usersService;
@@ -16,11 +17,17 @@ public class InsertSampleDataService {
 	@PostConstruct
 	public void init() {
 		User user1 = new User("99999990A", "Pedro", "Díaz");
+		user1.setPassword("123456");
 		User user2 = new User("99999991B", "Lucas", "Núñez");
+		user2.setPassword("123456");
 		User user3 = new User("99999992C", "María", "Rodríguez");
+		user3.setPassword("123456");
 		User user4 = new User("99999993D", "Marta", "Almonte");
+		user4.setPassword("123456");
 		User user5 = new User("99999977E", "Pelayo", "Valdes");
+		user5.setPassword("123456");
 		User user6 = new User("99999988F", "Edward", "Núñez");
+		user6.setPassword("123456");
 		Set user1Marks = new HashSet<Mark>() {
 			{
 				add(new Mark("Nota A1", 10.0, user1));
--- Notaneitor/src/main/java/com/uniovi/services/UsersService.java
+++ Notaneitor/src/main/java/com/uniovi/services/UsersService.java
@@ -3,6 +3,7 @@ package com.uniovi.services;
 import com.uniovi.entities.User;
 import com.uniovi.repositories.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -13,6 +14,9 @@ import java.util.List;
 public class UsersService {
 	@Autowired
 	private UsersRepository usersRepository;
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@PostConstruct
 	public void init() {
@@ -29,8 +33,14 @@ public class UsersService {
 	}
 
 	public void addUser(User user) {
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 		usersRepository.save(user);
 	}
+
+	public User getUserByDni(String dni) {
+		return usersRepository.findByDni(dni);
+	}
+
 
 	public void deleteUser(Long id) {
 		usersRepository.deleteById(id);
