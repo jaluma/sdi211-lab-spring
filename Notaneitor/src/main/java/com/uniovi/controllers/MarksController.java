a1782eb5e6b97a529cff00c0f821af62e158f6d0
Javi Martinez <javiermartinezalvarez98@gmail.com>
2019-02-12 17:19:20 +0100

    SDI-SpringBoot- 4 -Acceso a datos con entidades relacionadas
    Signed-off-by: Javi Martinez <javiermartinezalvarez98@gmail.com>

--- Notaneitor/src/main/java/com/uniovi/controllers/MarksController.java
+++ Notaneitor/src/main/java/com/uniovi/controllers/MarksController.java
@@ -2,6 +2,7 @@ package com.uniovi.controllers;
 
 import com.uniovi.entities.Mark;
 import com.uniovi.services.MarksService;
+import com.uniovi.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +16,9 @@ public class MarksController {
 
 	@Autowired //Inyectar el servicio
 	private MarksService marksService;
+
+	@Autowired
+	private UsersService usersService;
 
 	@RequestMapping("/mark/list")
 	public String getList(Model model) {
@@ -41,20 +45,25 @@ public class MarksController {
 	}
 
 	@RequestMapping(value = "/mark/add")
-	public String getMark() {
+	public String getMark(Model model) {
+		model.addAttribute("usersList", usersService.getUsers());
 		return "mark/add";
 	}
 
 	@RequestMapping(value = "/mark/edit/{id}")
 	public String getEdit(Model model, @PathVariable Long id) {
 		model.addAttribute("mark", marksService.getMark(id));
+		model.addAttribute("usersList", usersService.getUsers());
 		return "mark/edit";
 	}
 
 	@RequestMapping(value = "/mark/edit/{id}", method = RequestMethod.POST)
 	public String setEdit(Model model, @PathVariable Long id, @ModelAttribute Mark mark) {
-		mark.setId(id);
-		marksService.addMark(mark);
+		Mark original = marksService.getMark(id);
+		// modificar solo score y description
+		original.setScore(mark.getScore());
+		original.setDescription(mark.getDescription());
+		marksService.addMark(original);
 		return "redirect:/mark/details/" + id;
 	}
 
--- Notaneitor/src/main/java/com/uniovi/entities/Mark.java
+++ Notaneitor/src/main/java/com/uniovi/entities/Mark.java
@@ -1,8 +1,6 @@
 package com.uniovi.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Mark {
@@ -11,16 +9,25 @@ public class Mark {
 	private Long id;
 	private String description;
 	private Double score;
-
-
-	public Mark() {
-	}
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
 
 	public Mark(Long id, String description, Double score) {
 		super();
 		this.id = id;
 		this.description = description;
 		this.score = score;
+	}
+
+	public Mark(String description, Double score, User user) {
+		super();
+		this.description = description;
+		this.score = score;
+		this.user = user;
+	}
+
+	public Mark() {
 	}
 
 	public Long getId() {
@@ -47,8 +54,11 @@ public class Mark {
 		this.score = score;
 	}
 
-	@Override
-	public String toString() {
-		return "Mark [id=" + id + ", description=" + description + ", score=" + score + "]";
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
 	}
 }
